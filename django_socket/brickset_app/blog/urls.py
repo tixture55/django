7d2e18b4cb5a52a27604f90f2919925165961d87
frank marks <hdx48s@yahoo.co.jp>
2018-03-18 14:55:50 +0900

    validation form added

--- django_socket/brickset_app/blog/urls.py
+++ django_socket/brickset_app/blog/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 
 urlpatterns = [
     #url(r'^$', views.post_list, name='post_list'),
-    url(r'^', views.post_list, name='post_list'),
-    #url(r'^items/$', ListView.as_view()),
+    url(r'^template/$', views.hello_template, name='hello_template'),
+    url(r'^get/$', views.hello_get_query, name='hello_get_query'), 
+    url(r'^$', views.hello_forms, name='hello_forms'),
 ]
--- django_socket/brickset_app/blog/views.py
+++ django_socket/brickset_app/blog/views.py
@@ -9,9 +9,10 @@ from django.views import generic
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.db import models
-from django.db.models import Prefetch
 from blog.models import Member
 from django.shortcuts import render
+from . import forms
+
 
 # Create your views here.
 
@@ -23,3 +24,25 @@ class MemberListView(generic.ListView):
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def hello_forms(request):
+    form = forms.HelloForm(request.GET or None)
+    if form.is_valid():
+        message = 'data inspection is success!'
+    else:
+        message = 'data inspection is failed!'
+    d = {
+        'form': form,
+        'message': message,
+    }
+    return render(request, 'forms.html', d)
+
+def hello_template(request):
+    return render(request, 'index.html')
+
+def hello_get_query(request):
+    d = {
+        'your_name': request.GET.get('your_name')
+    }
+    return render(request, 'get_query.html', d)
